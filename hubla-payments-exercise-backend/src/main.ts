7f82f2fed1ec62fea5e9c319709ec9ff8f5d5ddd
Gustavo Rocha Dias <gustrd@gmail.com>
2023-03-11 20:05:27 -0300

    feat - Installation of Swagger and auxiliar packages.

--- hubla-payments-exercise-backend/src/main.ts
+++ hubla-payments-exercise-backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { configService } from './config/config.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Validation pipes configuration, used by DTOs
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }));
+  
+  //Swagger configuration, default url /docs
+  if (!configService.isProduction()) 
+  {
+    const document = SwaggerModule.createDocument(app, new DocumentBuilder()
+      .setTitle('Transaction API')
+      .setDescription('API to view and modify the sell transactions, used at the reports.')
+      .build());
+
+    SwaggerModule.setup('docs', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
