4af56402b2ebe15bb4660398e0d87c78ae3f3a3a
gustrd <gustrd@gmail.com>
2023-03-15 17:45:58 -0300

    fix - Correct import syntax.

--- hubla-payments-exercise-backend/src/scripts/write-type-orm-config.ts
+++ hubla-payments-exercise-backend/src/scripts/write-type-orm-config.ts
@@ -1,5 +1,6 @@
 import { configService } from '../config/config.service';
-import fs = require('fs');
+import  * as fs from 'fs';
+
 fs.writeFileSync('ormconfig.json',
     JSON.stringify(configService.getTypeOrmConfig(), null, 2)
 );
