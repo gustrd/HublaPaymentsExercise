8bc0506ebbdf027cbaec70c9c0611f82c25c2e92
Gustavo Rocha Dias <gustrd@gmail.com>
2023-03-16 19:43:23 -0300

    feat - Docker - Frontend - Creation of docker configuration to allow the front-end to run with docker-compose.

--- hubla-payments-exercise-backend/src/main.ts
+++ hubla-payments-exercise-backend/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   //CORS configuration
   app.enableCors(
     {
-      "origin": ["http://localhost:3000","http://localhost:3001"],
+      "origin": ["http://localhost:3000","http://localhost:3001","http://nodejs-hubla-frontend:3001","http://127.0.0.1:3001"],
       "methods": ["GET,HEAD,PUT,PATCH,POST,DELETE"],
       "credentials": true,
       "optionsSuccessStatus": 204,
